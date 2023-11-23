3811cc2839b5019c21a8b79b48251d86c195f700
Vilker Rainer <vilkerrainer1@gmail.com>
2023-11-23 19:13:16 -0300

    nada não

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+let nomeP1 = document.getElementById("per1");
+
 function personalizarPerfil() {
     window.location.href = "personalizar.html";
+}
+
+function personalizarPerfil() {
+    const nomeP1 = document.getElementById("per1").innerText;
+    localStorage.setItem("nomePerfil", nomeP1);
+    window.location.href = "segunda.html";
 }
