88e0d7eb4d277609b7230caa49ab12b9c58f1a7d
Vilker Rainer <vilkerrainer1@gmail.com>
2024-02-01 20:15:43 -0300

    nÃO LEMBRO

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+let nomeDoPerfil1 = "";
+
+
 function personalizarPerfil() {
     window.location.href = "personalizar.html";
 }
 
+function mudarNome() {
+    nomeDoPerfil1 = document.getElementById("novoNomeDoPerfil");
+}
+
 function salvarPerfil() {
     window.location.href = "index.html";
+    mudarNome();
 }
+
+function nomeDoPerfil () {
+document.getElementById("per1").innerHTML  = nomeDoPerfil1;
+}
+
